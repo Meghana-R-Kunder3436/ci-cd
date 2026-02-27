6c440ea2a9acf6b393490a45251713d1aef02c4b
Megahana <megahana>
2026-02-27 12:38:05 +0530

    Updated portfolio UI and styles

--- app.py
+++ app.py
@@ -1,15 +1,4 @@
-# import os
-# from flask import Flask
 
-# app = Flask(__name__)
-
-# @app.route("/")
-# def home():
-#     return "Hello from Railway with Gunicorn!"
-
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 8080))
-#     app.run(host="0.0.0.0", port=port)
 
 from flask import Flask, render_template
 import os
