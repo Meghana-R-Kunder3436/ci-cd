c29afdffd9cb06fe4be12cc4f37334d595761e87
Megahana <megahana>
2026-02-27 11:18:21 +0530

    Initial portfolio website

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask
+# import os
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def home():
+#     return "Hello from Railway with Gunicorn!"
+
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 8080))
+#     app.run(host="0.0.0.0", port=port)
+
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Railway!"
+    return render_template("index.html")  # Load your portfolio page
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
